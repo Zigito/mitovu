f76aa7e855b55b0e74a4e2f84a1df15cadb3655d
oaghedo <o.aghedo@gmail.com>
2018-10-08 08:18:02 +0100

    changed welcome message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
- res.send('Mitovu to the world');
+ res.send('Welcome To Mitovu');
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
