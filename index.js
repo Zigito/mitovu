70d8525c59a655bdcf4487c304992088d1429e28
oaghedo <o.aghedo@gmail.com>
2018-10-09 18:18:02 +0100

    more udpdates

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express();
 app.get('/', function (req, res) {
  res.send('Mitovu App');
 });
+
+app.get('/api/courses', function (req, res) {
+    res.send({
+        courses: [{
+            'name' : 'Algebra',
+        },
+        {
+            'name': 'Geometry',
+        },
+        {
+            'name': 'Calculus',
+        },
+    ]
+    });
+});
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 
