03afe6ee2d419547d31a732c73e863bfcbafdfbd
oaghedo <o.aghedo@gmail.com>
2018-10-09 15:41:16 +0100

    more udpdates

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const courses = require('./routes/courses');
+const homes = require('./routes/home');
 
 const express = require('express');
 const app = express();
 
+app.use('/', home);
+app.use('/api/courses', courses);
 //Respond with "hello world" for requests that hit our root "/"
 /* app.get('/', async (req, res) => {
     res.send('Mitovu to the world');
