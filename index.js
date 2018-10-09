f7132af70b8a4eca9497da78a49e8f78e8045732
oaghedo <o.aghedo@gmail.com>
2018-10-09 13:28:43 +0100

    Intsalled dependencies : Joi

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 
 const mongoose = require('mongoose');
 const Joi = require('joi');
-const morgan = require('morgan'); //log http requests
 const home = require('./routes/home');
 const courses = require('./routes/courses');
 const express = require('express');
 const app = express();
 
 
-mongoose.connect('mongodb://localhost/okestra')
+/* mongoose.connect('mongodb://localhost/okestra')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
-
+ */
 
 app.use('/', home);
 app.use('/api/courses', courses);// for any route that starts with /api/courses use the courses router
