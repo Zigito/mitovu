d45509f98cbdee6373ffd9403a4941aeb8dc87a6
oaghedo <o.aghedo@gmail.com>
2018-10-09 13:56:51 +0100

    fixed issue with mongoose

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ const express = require('express');
 const app = express();
 
 
-/* mongoose.connect('mongodb://localhost/okestra')
+mongoose.connect('mongodb://kogoradev:/79Uc^7P&==G6\'Z@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
- */
+
 
 app.use('/', home);
 app.use('/api/courses', courses);// for any route that starts with /api/courses use the courses router
