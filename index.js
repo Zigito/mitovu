a218fe47984a3bb90254fb317978559f69880705
oaghedo <o.aghedo@gmail.com>
2018-11-01 04:20:02 +0100

    created course schema

--- index.js
+++ index.js
@@ -1,35 +1,9 @@
 var express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
+mongoose.connect('mongodb://kogoradev:/CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
 
-//defines the shape of course documents
-const courseSchema = new mongoose.Schema({
-    name: String,
-    author: String,
-    tags: [String],
-    date: { type: Date, default: Date.now },
-    isPublished: Boolean
-});
-
-
-const Course = mongoose.model('Course', courseSchema);
-
-
-async function createCourse() {
-    const course = new Course({
-        name: 'Geometry',
-        author: 'Osagie',
-        tags: ['shapes', '3d', '2d'],
-        isPublished: true
-    });
-
-    const result = await course.save();
-    //console.log(result);
-}
-
-var courseResult  = createCourse();
 
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
