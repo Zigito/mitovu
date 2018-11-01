30f46832835595dc9baaee6959067655a9f875fe
oaghedo <o.aghedo@gmail.com>
2018-11-01 01:51:07 +0100

    'changes'

--- test/test.js
+++ test/test.js
@@ -2,16 +2,8 @@ const request = require('supertest');
 const app = require('../index.js');
 describe('GET /', function () {
     it('respond with Welcome To Mitovu', function(done) {
-            //navigate to root and check the the response is "Mitovu App"
+            //navigate to root and check the the response is "hello world"
         request(app).get('/').expect('Mitovu App', done);
  });
 });
 
-var request = require(‘supertest’);
-var app = require(‘../index.js’);
-describe(‘GET /’, function () {
-        it(‘respond with hello world’, function(done) {
-            //navigate to root and check the the response is "hello world"
-            request(app).get(‘/’).expect(‘hello world’, done);
- });
-});
