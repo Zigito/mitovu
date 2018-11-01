f299d232d61505621ab640f52154d6c7ffea6d6c
oaghedo <o.aghedo@gmail.com>
2018-11-01 03:47:33 +0100

    created course schema

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://kogoradev:/79Uc^7P&==G6\'Z@206.189.17.119:27017/mitovu')
+mongoose.connect('mongodb://kogoradev:/CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
 
@@ -77,15 +77,22 @@ async function createCourse() {
     }
 }
 
-
-
-
-
+async function getCourses() {
+    const courses = await Course.find();
+}
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
     res.send('Mitovu App');
 });
+
+//Respond with "hello world" for requests that hit our root "/"
+/* app.get('/courses', function (req, res) {
+    const courses = getCourses();
+    res.send(courses);
+}); */
+
+
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
 
