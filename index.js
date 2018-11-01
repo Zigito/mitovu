03bfad23c41b5c2aad0e67f5caa4791350804c41
oaghedo <o.aghedo@gmail.com>
2018-11-01 04:09:14 +0100

    created course schema

--- index.js
+++ index.js
@@ -6,86 +6,34 @@ mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu
 
 //defines the shape of course documents
 const courseSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-        lowercase: true,
-        minlength: 5,
-        maxlength: 255,
-        trim: true
-        //match: //pattern
-    },
+    name: String,
     author: String,
-    tags: {
-        type: Array,
-        validate: {
-            isAsync: true,
-            validator: function (v, callback) {
-                setTimeout(() => {
-                    //do some async work
-                    const result = v && v.length > 0;
-                    callback(result)
-                }, 1);
-            },
-            message: 'A course should have at least one tag'
-        }
-    },
-    category: {
-        type: String,
-        required: true,
-        enum: ['elementary', 'middle', 'high'],
-        lowercase: true,
-        //uppercase: true,
-        trim: true
-    },
     tags: [String],
     date: { type: Date, default: Date.now },
-    isPublished: Boolean,
-    price: {
-        type: Number,
-        required: function () { return this.isPublished; },        //price is only required if if published is true
-        min: 10,
-        max: 200,
-        get: v => Math.round(v),//custom getter
-        set: v => Math.round(v)
-    },
-
+    isPublished: Boolean
 });
 
-//create the data model
+
 const Course = mongoose.model('Course', courseSchema);
+
 
 async function createCourse() {
     const course = new Course({
-        name: 'Algebra',
+        name: 'Geometry',
         author: 'Osagie',
-        category: 'middle',
-        tags: ['conceptual', 'stem'],
-        isPublished: true,
-        price: 30.50
+        tags: ['shapes', '3d', '2d'],
+        isPublished: true
     });
 
-    try {
-        const result = await course.save();
-        console.log(result);
-    }
-    catch (ex) {
-        //iterating over the validation error object
-        for (field in ex.errors)
-            console.log(ex.errors[field].message);
-        //console.log(ex.message);
-    }
+    const result = await course.save();
+    console.log(result);
 }
 
-async function getCourses() {
-    const courses = await Course.find();
-}
-
+createCourse();
 
 var app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
-    createCourse();
     res.send('Mitovu App');
 });
 
