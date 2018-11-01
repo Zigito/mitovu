d296cbf6ef704baff293236dabe38324e0183a19
oaghedo <o.aghedo@gmail.com>
2018-11-01 03:53:04 +0100

    created course schema

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://kogoradev:/CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
+mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
 
