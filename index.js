913955afcf0d1d9dad311f45cfa3d4db5a267d65
oaghedo <o.aghedo@gmail.com>
2018-11-03 12:36:20 +0100

    new changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const _ = require('lodash');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
@@ -10,6 +11,33 @@ var app = express();
 app.get('/', function (req, res) {
     res.send('Mitovu App');
 });
+user = new User(_.pick(req.body, ['name', 'email', 'password']));
+
+
+router.post('/', (req, res) => {
+
+    //input  validation
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    res.send(course);
+});
+
+
+// Course Validation
+function validateCourse(course) {
+
+    //define a Joi schema for validation
+    const schema = {
+        name: Joi.string().min(5).required()
+        //   level: Joi.number().min().required()
+    };
+
+    return Joi.validate(course, schema);
+}
+
 
 //Respond with "hello world" for requests that hit our root "/"
 /* app.get('/courses', function (req, res) {
