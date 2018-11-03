dcf2bf88fdbb32cff15d977b0e9972b4c07786e8
oaghedo <o.aghedo@gmail.com>
2018-11-03 12:51:20 +0100

    new changes

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 var express = require('express');
-//const Joi = require('joi');
-//const _ = require('lodash');
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
@@ -13,30 +11,12 @@ app.get('/', function (req, res) {
     res.send('Mitovu App');
 });
 
-/* app.post('/', function (req, res)  {
+//Respond with "hello world" for requests that hit our root "/"
+/* app.get('/courses', function (req, res) {
+    const courses = getCourses();
+    res.send(courses);
+}); */
 
-    //input  validation
-    const { error } = validateCourse(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
-
-    res.send(course);
-});
-
-
-// Course Validation
-function validateCourse(course) {
-
-    //define a Joi schema for validation
-    const schema = {
-        name: Joi.string().min(5).required()
-        //   level: Joi.number().min().required()
-    };
-
-    return Joi.validate(course, schema);
-}
- */
 
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
