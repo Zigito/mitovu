ef5340c7c743560023ad1317f2d7aa7c202673d1
oaghedo <o.aghedo@gmail.com>
2018-11-03 12:09:19 +0100

    New changes

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ mongoose.connect('mongodb://kogoradev:/CnxwQbK7SykLRq@206.189.17.119:27017/mitov
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
 
-
-const app = express();
 //Respond with "hello world" for requests that hit our root "/"
 app.get('/', function (req, res) {
     res.send('Mitovu App');
@@ -22,3 +20,4 @@ app.get('/', function (req, res) {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
+
