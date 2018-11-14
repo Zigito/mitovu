3c0fbb2f2cee4ef3bea972a07af5cb5e2696625b
oaghedo <o.aghedo@gmail.com>
2018-11-14 13:34:04 +0100

    new changes

--- routes/courses.js
+++ routes/courses.js
@@ -1,43 +1,55 @@
-//const Joi = require('joi');
-const { Customer, validate } = require('../models/customer'); 
-const mongoose = require('mongoose');
-const _ = require('lodash');
+const Joi = require('joi');
 var express = require('express');
-const router = express.Router();
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
+    .then(() => console.log('Connected to MongoDb...'))
+    .catch(err => console.error('Could not connect to MongoDb', err));
+
+var app = express();
+
+app.use(express.json());
 
 //Respond with "hello world" for requests that hit our root "/"
-router.get('/', async (req, res) => {
-    res.send('Welcome To Sitovu');
+app.get('/', function (req, res) {
+    res.send('Mitovu App');
 });
 
 
-router.post('/', async (req, res) => {
+app.post('/', function (req, res) {
+
     //input  validation
-    const { error } = validate(req.body);
-    if (error)  return res.status(400).send(error.details[0].message);
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
 
-
-    let course = new Course({
+    const course = {
         name: req.body.name,
         category: req.body.category
-    });
-
-    course = await course.save();
+    };
 
     res.send(course);
 });
 
 
-/* function validateCourse(course) {
+function validateCourse(course) {
 
     //define a Joi schema for validation
     const schema = {
         name: Joi.string().min(5).required(),
-        category: Joi.string().min().required()
+        category: Joi.string().min(5).required()
     };
 
     return Joi.validate(course, schema);
-} */
+}
+//Respond with "hello world" for requests that hit our root "/"
+/* app.get('/courses', function (req, res) {
+    const courses = getCourses();
+    res.send(courses);
+}); */
 
 
-module.exports = router; 
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,8 @@
 const request = require('supertest');
 const app = require('../index.js');
 describe('GET /', function () {
-    it('respond with Welcome To Sitovu', function(done) {
-            //navigate to root and check the the response is "hello world"
-        request(app).get('/').expect('Welcome To Sitovu', done);
- });
+    it('respond with Welcome To Mitovu', function (done) {
+        //navigate to root and check the the response is "hello world"
+        request(app).get('/').expect('Mitovu App', done);
+    });
 });
-
