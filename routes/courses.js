12495e7db47c893fb664083c667805b80e694337
oaghedo <o.aghedo@gmail.com>
2018-11-14 13:41:26 +0100

    new changes

--- routes/courses.js
+++ routes/courses.js
@@ -1,55 +1,43 @@
-const Joi = require('joi');
-var express = require('express');
+//const Joi = require('joi');
+const { Customer, validate } = require('../models/customer'); 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
-    .then(() => console.log('Connected to MongoDb...'))
-    .catch(err => console.error('Could not connect to MongoDb', err));
-
-var app = express();
-
-app.use(express.json());
+const _ = require('lodash');
+var express = require('express');
+const router = express.Router();
 
 //Respond with "hello world" for requests that hit our root "/"
-app.get('/', function (req, res) {
-    res.send('Mitovu App');
+router.get('/', async (req, res) => {
+    res.send('Welcome To Sitovu');
 });
 
 
-app.post('/', function (req, res) {
-
+router.post('/', async (req, res) => {
     //input  validation
-    const { error } = validateCourse(req.body);
-    if (error) {
-        return res.status(400).send(error.details[0].message);
-    }
+    const { error } = validate(req.body);
+    if (error)  return res.status(400).send(error.details[0].message);
 
-    const course = {
+
+    let course = new Course({
         name: req.body.name,
         category: req.body.category
-    };
+    });
+
+    course = await course.save();
 
     res.send(course);
 });
 
 
-function validateCourse(course) {
+/* function validateCourse(course) {
 
     //define a Joi schema for validation
     const schema = {
         name: Joi.string().min(5).required(),
-        category: Joi.string().min(5).required()
+        category: Joi.string().min().required()
     };
 
     return Joi.validate(course, schema);
-}
-//Respond with "hello world" for requests that hit our root "/"
-/* app.get('/courses', function (req, res) {
-    const courses = getCourses();
-    res.send(courses);
-}); */
+} */
 
 
-//listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
-
-module.exports = app;
+module.exports = router; 
