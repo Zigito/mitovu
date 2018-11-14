0c51499fc05c287200e6c070ce6a84f2c022b0b5
oaghedo <o.aghedo@gmail.com>
2018-11-14 14:16:39 +0100

    new changes

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const Joi = require('joi');
-const courses = require('./routes/courses');
 var express = require('express');
 const mongoose = require('mongoose');
-
-
 mongoose.connect('mongodb://kogoradev:CnxwQbK7SykLRq@206.189.17.119:27017/mitovu')
     .then(() => console.log('Connected to MongoDb...'))
     .catch(err => console.error('Could not connect to MongoDb', err));
 
-
-
 var app = express();
 
 app.use(express.json());
-app.use('/api/customers', courses);
+
+//Respond with "hello world" for requests that hit our root "/"
+app.get('/', function (req, res) {
+    res.send('Mitovu App');
+});
 
 
+app.post('/', function (req, res) {
 
+    //input  validation
+    const { error } = validateCourse(req.body);
+    if (error) {
+        return res.status(400).send(error.details[0].message);
+    }
+
+    const course = {
+        name: req.body.name,
+        category: req.body.category
+    };
+
+    res.send(course);
+});
+
+
+function validateCourse(course) {
+
+    //define a Joi schema for validation
+    const schema = {
+        name: Joi.string().min(5).required(),
+        category: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 //Respond with "hello world" for requests that hit our root "/"
 /* app.get('/courses', function (req, res) {
     const courses = getCourses();
@@ -28,4 +53,3 @@ app.use('/api/customers', courses);
 app.listen(process.env.PORT || 3000);
 
 module.exports = app;
-
